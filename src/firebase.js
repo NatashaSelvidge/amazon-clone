3f0206a369eeae74e890c9b707c37a4673c609c5
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-05-17 15:19:23 -0700

    added firebase info

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDWYf9nvuV3YKVKOCr-jLnXlLIGiKcZnZM",
+  authDomain: "clone-ddda7.firebaseapp.com",
+  projectId: "clone-ddda7",
+  storageBucket: "clone-ddda7.appspot.com",
+  messagingSenderId: "644289857913",
+  appId: "1:644289857913:web:54f69b6f7d5641dad747a8",
+  measurementId: "G-6NVE8MH4ZH",
+};
